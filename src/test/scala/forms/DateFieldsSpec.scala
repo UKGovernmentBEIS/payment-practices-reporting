9308ee194cbdb39e96dbe27ee41dbed0a5d0a7f0
Doug Clinton <doug@wellfactored.com>
2017-02-03 10:23:23 +0000

    Fixed the DateFieldsSpec

--- src/test/scala/forms/DateFieldsSpec.scala
+++ src/test/scala/forms/DateFieldsSpec.scala
@@ -1,16 +1,28 @@
-package forms.report
+/*
+ * Copyright (C) 2017  Department for Business, Energy and Industrial Strategy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-import forms.DateFields
+package forms
+
 import org.joda.time.LocalDate
 import org.scalatest.{EitherValues, Matchers, WordSpecLike}
 import play.api.data.FormError
-import utils.SystemTimeSource
 
 class DateFieldsSpec extends WordSpecLike with Matchers with EitherValues {
-  val validations = new Validations(new SystemTimeSource)
-
-  import validations.dateFields
-  import validations.dateFromFields
+  import Validations.{dateFields, dateFromFields}
 
   def errorFor(fieldName: String) = List(FormError(fieldName, List("error.number")))
 
