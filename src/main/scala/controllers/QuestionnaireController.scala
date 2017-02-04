aa25f15d24af08312932ee5e6b01e579599c1f8a
Doug Clinton <doug@wiredthing.com>
2017-02-04 22:33:38 +0000

    Started buidling up form mappings for the decision state

--- src/main/scala/controllers/QuestionnaireController.scala
+++ src/main/scala/controllers/QuestionnaireController.scala
@@ -19,18 +19,40 @@ package controllers
 
 import javax.inject.Inject
 
+import play.api.data.Forms._
+import play.api.data.Mapping
 import play.api.i18n.MessagesApi
 import play.api.mvc.{Action, Controller}
-import questionnaire.FinancialYear
+import questionnaire.{DecisionState, FinancialYear, Thresholds, YesNo}
 
 class QuestionnaireController @Inject()(implicit messages: MessagesApi) extends Controller with PageHelper {
+
+  val yesNo: Mapping[YesNo] = ???
+  val financialYear: Mapping[FinancialYear] = ???
+  val thresholds: Mapping[Thresholds] = mapping(
+    "turnover" -> optional(yesNo),
+    "balanceSheet" -> optional(yesNo),
+    "employees" -> optional(yesNo)
+  )(Thresholds.apply)(Thresholds.unapply)
+
+  val decisionStateMapping: Mapping[DecisionState] = mapping(
+    "isCompanyOrLLP" -> optional(yesNo),
+    "financialYear" -> optional(financialYear),
+    "companyThresholds" -> thresholds,
+    "subsidiaries" -> optional(yesNo),
+    "subsidiaryThresholds" -> thresholds
+  )(DecisionState.apply)(DecisionState.unapply)
+
+  val emptyState = decisionStateMapping.unbind(DecisionState.empty)
 
   import controllers.routes.{QuestionnaireController => routeTo}
   import views.html.{questionnaire => pages}
 
   private val exemptReasonKey = "exempt_reason"
 
-  def start = Action(Ok(page(home, views.html.questionnaire.start())))
+  def start = Action { implicit request =>
+    Ok(page(home, views.html.questionnaire.start())).addingToSession(emptyState.toSeq: _*)
+  }
 
   def companyOrLLC = Action(Ok(page(home, pages.companyOrLLC())))
 
