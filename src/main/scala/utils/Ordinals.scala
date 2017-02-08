033d57f6e38211696bd56515943dc26cb63510e7
Doug Clinton <doug@wiredthing.com>
2017-02-08 08:19:07 +0000

    Optimised import

--- src/main/scala/utils/Ordinals.scala
+++ src/main/scala/utils/Ordinals.scala
@@ -19,7 +19,7 @@ package utils
 
 import eu.timepit.refined._
 import eu.timepit.refined.api.Refined
-import eu.timepit.refined.numeric.{Positive, _}
+import eu.timepit.refined.numeric._
 
 object Ordinals {
 
