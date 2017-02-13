5f3ebf847cd04d6b3aeef72e7c80239fc81e8c70
Doug Clinton <doug@wellfactored.com>
2017-02-13 13:21:30 +0000

    Added an additional test for CSV quoting

--- src/test/scala/controllers/ReportCSV$Test.scala
+++ src/test/scala/controllers/ReportCSV$Test.scala
@@ -12,6 +12,13 @@ class ReportCSV$Test extends WordSpecLike with Matchers {
       ReportCSV.escape(s) shouldBe expected
     }
 
+    "double up two double-quote characters and quote the string" in {
+      val s = """ "foo" "bar" """
+      val expected = """" ""foo"" ""bar"" """"
+
+      ReportCSV.escape(s) shouldBe expected
+    }
+
     "quote a string with a newline" in {
       val s = "foo\nbar"
       val expected = "\"foo\nbar\""
