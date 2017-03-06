c4af3e5c9420f1bcec22271b0d0000435754a9b6
Doug Clinton <doug@wellfactored.com>
2017-03-06 10:00:01 +0000

    Small re-factorings

--- src/main/assets/javascripts/disclosure.js
+++ src/main/assets/javascripts/disclosure.js
@@ -1,7 +1,7 @@
 /* Gradual disclosure */
 
 function gradualDisclosure() {
-    var data =[
+    var data = [
         ["show-if-payment-codes", "paymentCodes.yesNo"],
         ["show-if-payment-changes", "paymentTerms.paymentTermsChanged.changed.yesNo"],
         ["show-if-payment-changes-notified", "paymentTerms.paymentTermsChanged.notified.yesNo"]
@@ -10,19 +10,13 @@ function gradualDisclosure() {
     function showPanelIfYes(panelId, checkboxName) {
         var panel = document.getElementById(panelId);
         var radios = document.getElementsByName(checkboxName);
-        var val = "no";
+        var yesNo = "no";
 
         for (var i = 0; i < radios.length; i++) {
-            if (radios[i].checked) {
-                val = radios[i].value;
-            }
+            if (radios[i].checked) yesNo = radios[i].value;
         }
 
-        if (val === "yes") {
-            panel.style.display = "";
-        } else {
-            panel.style.display = "none";
-        }
+        panel.style.display = yesNo === "yes" ? "" : "none";
     }
 
     function subscribeToChange(panelId, checkboxName) {
@@ -35,7 +29,10 @@ function gradualDisclosure() {
     }
 
     for (var j = 0; j < data.length; j++) {
-        showPanelIfYes(data[j][0], data[j][1]);
-        subscribeToChange(data[j][0], data[j][1]);
+        var panelId = data[j][0];
+        var checkboxName = data[j][1];
+
+        showPanelIfYes(panelId, checkboxName);
+        subscribeToChange(panelId, checkboxName);
     }
 }
