98b8843a7ce4a224fd5e87337184b90a51dd71a0
Doug Clinton <doug@wellfactored.com>
2017-03-16 10:20:45 +0000

    Added more tests

--- src/test/scala/questionnaire/DeciderTest.scala
+++ src/test/scala/questionnaire/DeciderTest.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2017  Department for Business, Energy and Industrial Strategy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package questionnaire
 
 import org.scalatest.prop.TableDrivenPropertyChecks
@@ -21,22 +38,33 @@ object DeciderTestData {
   import utils.YesNo._
 
   val empty = DecisionState.empty
+  val s0 = empty.copy(isCompanyOrLLP = Some(No))
   val s1 = empty.copy(isCompanyOrLLP = Some(Yes))
+
   val s2a = s1.copy(financialYear = Some(FinancialYear.First))
 
   val s2b = s1.copy(financialYear = Some(FinancialYear.Second))
-  val s3b = s2b.copy(companyThresholds = Thresholds.empty.copy(turnover = Some(Yes)))
+  val s3b = s2b.copy(companyThresholds = s2b.companyThresholds.copy(turnover = Some(Yes)))
+  val s4b = s3b.copy(companyThresholds = s3b.companyThresholds.copy(balanceSheet = Some(Yes)))
 
   val s2c = s1.copy(financialYear = Some(FinancialYear.ThirdOrLater))
+  val s3c = s2c.copy(companyThresholds = s2c.companyThresholds.copy(turnover = Some(Yes)))
+  val s4c = s3c.copy(companyThresholds = s3c.companyThresholds.copy(balanceSheet = Some(Yes)))
 
   val expectedDecisions: Seq[(DecisionState, Decision)] = Seq(
     (empty, AskQuestion(Questions.isCompanyOrLLPQuestion)),
+    (s0, Exempt(None)),
+
     (s1, AskQuestion(Questions.financialYearQuestion)),
+
     (s2a, Exempt(Some("reason.firstyear"))),
 
     (s2b, AskQuestion(Questions.companyTurnoverQuestionY2)),
     (s3b, AskQuestion(Questions.companyBalanceSheetQuestionY2)),
+    (s4b, AskQuestion(Questions.hasSubsidiariesQuestion)),
 
-    (s2c, AskQuestion(Questions.companyTurnoverQuestionY3))
+    (s2c, AskQuestion(Questions.companyTurnoverQuestionY3)),
+    (s3c, AskQuestion(Questions.companyBalanceSheetQuestionY3)),
+    (s4c, AskQuestion(Questions.hasSubsidiariesQuestion))
   )
 }
