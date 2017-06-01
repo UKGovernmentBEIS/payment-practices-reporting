1813debb2de4fec6529e5f20a1f32dee8cbfabc1
Doug Clinton <doug@wellfactored.com>
2017-06-01 15:05:22 +0100

    Tweaked the way data is stashed in the ReportingPeriodController

--- src/main/scala/controllers/ReportingPeriodController.scala
+++ src/main/scala/controllers/ReportingPeriodController.scala
@@ -22,9 +22,8 @@ import javax.inject.{Inject, Named}
 import actions.{CompanyAuthAction, CompanyAuthRequest}
 import akka.actor.ActorRef
 import config.{PageConfig, ServiceConfig}
-import forms.report.{LongFormModel, ShortFormModel, ReportingPeriodFormModel, Validations}
+import forms.report.Validations
 import models.CompaniesHouseId
-import play.api.data.Form
 import play.api.i18n.MessagesApi
 import play.api.mvc.Controller
 import play.twirl.api.Html
@@ -36,26 +35,24 @@ class ReportingPeriodController @Inject()(
                                            reports: ReportService,
                                            validations: Validations,
                                            companyAuth: CompanyAuthService,
-                                           CompanyAuthAction: CompanyAuthAction,
+                                           companyAuthAction: CompanyAuthAction,
                                            val serviceConfig: ServiceConfig,
                                            val pageConfig: PageConfig,
                                            @Named("confirmation-actor") confirmationActor: ActorRef
                                          )(implicit ec: ExecutionContext, messages: MessagesApi) extends Controller with PageHelper {
 
-  import views.html.{report => pages}
-
-
   import validations._
+  import views.html.{report => pages}
 
   def reportPageHeader(implicit request: CompanyAuthRequest[_]): Html = h1(s"Publish a report for:<br>${request.companyDetail.companyName}")
   private def publishTitle(companyName: String) = s"Publish a report for $companyName"
   private def title(implicit request: CompanyAuthRequest[_]): String = publishTitle(request.companyDetail.companyName)
 
-  def start(companiesHouseId: CompaniesHouseId) = CompanyAuthAction(companiesHouseId) { implicit request =>
+  def start(companiesHouseId: CompaniesHouseId) = companyAuthAction(companiesHouseId) { implicit request =>
     Ok(page(title)(home, pages.reportingPeriod(reportPageHeader, emptyReportingPeriod, Map.empty, companiesHouseId, df, serviceStartDate)))
   }
 
-  def post(companiesHouseId: CompaniesHouseId) = CompanyAuthAction(companiesHouseId)(parse.urlFormEncoded) { implicit request =>
+  def post(companiesHouseId: CompaniesHouseId) = companyAuthAction(companiesHouseId)(parse.urlFormEncoded) { implicit request =>
     // Catch any stashed values for the main report (if we came back from the review page) but
     // we don't want to carry any errors forward when we progress to the next page. This also
     // makes sure that when the user starts filing a new report that the next page doesn't start
@@ -63,11 +60,14 @@ class ReportingPeriodController @Inject()(
     val longForm = emptyLongForm.bindForm.discardingErrors
     val shortForm = emptyShortForm.bindForm.discardingErrors
 
-    val reportingPeriodForm = emptyReportingPeriod.bindForm
+    // The longForm is a superset of the shortForm so its data will include all the fields we
+    // need to stash if we go back to the reportingPeriod page
+    val stashData: Map[String, String] = longForm.data
 
+    val reportingPeriodForm = emptyReportingPeriod.bindForm
     reportingPeriodForm.fold(
-      errs => BadRequest(page(title)(home, pages.reportingPeriod(reportPageHeader, errs, longForm.data ++ shortForm.data, companiesHouseId, df, serviceStartDate))),
-      reportingPeriod =>
+    errs => BadRequest(page(title)(home, pages.reportingPeriod(reportPageHeader, errs, stashData, companiesHouseId, df, serviceStartDate))),
+       reportingPeriod =>
         if (reportingPeriod.hasQualifyingContracts.toBoolean)
           Ok(page(title)(home, pages.longForm(reportPageHeader, longForm, reportingPeriodForm.data, companiesHouseId, df, serviceStartDate)))
         else
